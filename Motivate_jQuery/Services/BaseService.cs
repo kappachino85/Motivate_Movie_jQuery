df3e5237b470a27eac2086d0527eaed7ee2ba223
Jae Phan <kappachino85@gmail.com>
2018-05-30 19:20:43 -0700

    Implemented D.I., and set up the Services and Interfaces

---
+++ Motivate_jQuery/Services/BaseService.cs
@@ -0,0 +1,29 @@
+﻿using DatabaseConn.Adapter;
+using Motivate_jQuery.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Motivate_jQuery.Services
+{
+    public class BaseService : IBaseService
+    {
+        string _connectionString;
+
+        public BaseService()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+
+        public IDbAdapter SqlAdapter
+        {
+            get
+            {
+                return new DbAdapter(new SqlCommand(), new SqlConnection(_connectionString));
+            }
+        }
+    }
+}
